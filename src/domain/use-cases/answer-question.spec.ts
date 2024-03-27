5020a758b836902344674af1fdf765ad4a04c865
Jennifer Takagi <jennitakagi@gmail.com>
2024-03-27 10:51:06 +0100

    [feat] Add value object to slug

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -13,8 +13,8 @@ test('create an answer', async () => {
   const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
-    content: 'Nova resposta',
+    content: 'New answer',
   })
 
-  expect(answer.content).toEqual('Nova resposta')
+  expect(answer.content).toEqual('New answer')
 })
