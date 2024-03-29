bbbba552b0f03ec010abf3f7547db86c922b6014
Jennifer Takagi <jennitakagi@gmail.com>
2024-03-29 16:03:34 +0100

    [feat] Delete attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,21 +1,27 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { CreateQuestionUseCase } from './create-question'
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
 let sut: CreateQuestionUseCase
 
 describe('Create Question', () => {
   beforeEach(() => {
-    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
     sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
   })
 
   it('should be able to create a question', async () => {
     const result = await sut.execute({
       authorId: '1',
-      title: 'Nova pergunta',
-      content: 'Conteúdo da pergunta',
+      title: 'New question',
+      content: 'Answer content',
       attachmentsIds: ['1', '2'],
     })
 
--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -45,14 +45,14 @@ describe('Edit Question', () => {
     await sut.execute({
       questionId: newQuestion.id.toValue(),
       authorId: 'author-1',
-      title: 'Pergunta teste',
-      content: 'Conteúdo teste',
+      title: 'Question test',
+      content: 'Content test',
       attachmentsIds: ['1', '3'],
     })
 
     expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
-      title: 'Pergunta teste',
-      content: 'Conteúdo teste',
+      title: 'Question test',
+      content: 'Content test',
     })
 
     expect(
@@ -79,8 +79,8 @@ describe('Edit Question', () => {
     const result = await sut.execute({
       questionId: newQuestion.id.toValue(),
       authorId: 'author-2',
-      title: 'Pergunta teste',
-      content: 'Conteúdo teste',
+      title: 'Question test',
+      content: 'Content test',
       attachmentsIds: [],
     })
 
